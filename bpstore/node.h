905ca30c3e9def3023fd78d7368c168327261f54
lysevi <lysevi@gmail.com>
2019-08-01 13:42:20 +0300

    slice:move semantic.

--- bpstore/node.h
+++ bpstore/node.h
@@ -12,22 +12,25 @@ struct slice_t {
   slice_size_t size;
   uint8_t *data;
 
+  slice_t()
+      : size(0)
+      , data(nullptr) {}
+
   slice_t(slice_size_t sz, uint8_t *val)
       : size(sz)
       , data(val) {}
 
+  EXPORT slice_t(slice_t &&cp);
+  EXPORT void operator=(slice_t &&cp);
   EXPORT slice_t(const slice_t &cp);
   EXPORT void operator=(const slice_t &cp);
-
-  slice_t()
-      : size(0)
-      , data(nullptr) {}
 
   ~slice_t() {
     if (data != nullptr) {
       delete data;
       data = nullptr;
     }
+    size = 0;
   }
 
   // <  0 if this <  o,
