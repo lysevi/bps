041f5593fb8bae58191deea63cb714b075fd3414
lysevi <lysevi@gmail.com>
2019-08-02 09:28:29 +0300

    leaf.h

--- bpstore/tree.cpp
+++ bpstore/tree.cpp
@@ -1,8 +1,23 @@
 #include <bpstore/tree.h>
+#include <bpstore/utils/exception.h>
 
 using namespace bpstore;
 
+node_t::node_t(uint32_t cap)
+    : capacity(cap)
+    , size(0)
+    , keys(cap)
+    , children(cap) {}
+
+node_t::~node_t() {
+  size = 0;
+  keys.clear();
+  children.clear();
+}
+
 tree_t::tree_t(iblock_storage *storage)
-    : _storage(storage) {}
+    : _storage(storage) {
+  ENSURE(storage != nullptr);
+}
 
 tree_t::~tree_t() {}
--- bpstore/tree.h
+++ bpstore/tree.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <bpstore/exports.h>
-#include <bpstore/node.h>
+#include <bpstore/leaf.h>
 
 namespace bpstore {
 
@@ -9,6 +9,20 @@ struct iblock_storage {};
 
 struct tree_params_t {
   capacity_t node_capacity;
+};
+
+using storage_ptr_t = uint64_t;
+const storage_ptr_t k_storage_ptr_null = std::numeric_limits<storage_ptr_t>::max();
+
+struct node_t {
+  bool children_is_leaf = false;
+  uint32_t capacity;
+  uint32_t size;
+  std::vector<slice_t> keys;
+  std::vector<storage_ptr_t> children;
+
+  EXPORT node_t(uint32_t cap);
+  EXPORT ~node_t();
 };
 
 class tree_t {
