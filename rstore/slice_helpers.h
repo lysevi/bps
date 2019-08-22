2a410c12c30035db717604bc1fc3d92edbbae75f
lysevi <lysevi@gmail.com>
2019-08-22 13:53:29 +0300

    refact.

--- rstore/slice_helpers.h
+++ rstore/slice_helpers.h
@@ -8,7 +8,7 @@
 namespace rstore {
 
 template <typename T>
-T slice_convert_to(const Slice &sl) {
+std::enable_if_t<std::is_pod_v<T>, T> slice_convert_to(const Slice &sl) {
   static_assert(std::is_pod_v<T>);
   ENSURE(sizeof(T) == sl.size);
   T result = {};
@@ -17,7 +17,7 @@ T slice_convert_to(const Slice &sl) {
 }
 
 template <typename T>
-static Slice slice_make_from(const T &v, const slice_size_t s) {
+Slice slice_make_from(const T &v, const slice_size_t s) {
   static_assert(std::is_pod_v<T>);
 
   auto buf = new uint8_t[s];
@@ -27,8 +27,8 @@ static Slice slice_make_from(const T &v, const slice_size_t s) {
 }
 
 template <typename T>
-static std::enable_if_t<std::is_pod_v<T>, Slice> slice_make_from(const T &v) {
-  auto s = sizeof(T);
+std::enable_if_t<std::is_pod_v<T>, Slice> slice_make_from(const T &v) {
+  constexpr auto s = sizeof(T);
   return slice_make_from(v, static_cast<slice_size_t>(s));
 }
 } // namespace rstore
